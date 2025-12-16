12f60be75c8949cc089353adc222a7d0486e74b7
Dylan <carringtonfair.dylan@gmail.com>
2025-12-15 19:29:25 -0500

    attempt to fix how input was trimmed

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,17 @@ fn main() {
         print!("$ ");
         io::stdout().flush().unwrap();
 
-        let mut command = String::new();
-        io::stdin().read_line(&mut command).unwrap();
-        match command.trim().split_once(" ") {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        let trimmed_input = input.trim();
+        match trimmed_input.split_once(" ") {
             Some((command, arguments)) => match command {
                 "echo" => println!("{}", arguments),
                 _ => println!("{}: command not found", command),
             },
-            None => match command.trim() {
+            None => match trimmed_input {
                 "exit" => break,
-                _ => println!("{}: command not found", command),
+                _ => println!("{}: command not found", trimmed_input),
             },
         }
     }
