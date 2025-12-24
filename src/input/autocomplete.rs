b7e897bdd916642d2c45fe691c6bd7b750c51297
Dylan <carringtonfair.dylan@gmail.com>
2025-12-24 10:34:26 -0500

    can handle multiple matches now

--- src/input/autocomplete.rs
+++ src/input/autocomplete.rs
@@ -6,6 +6,8 @@ use std::os::unix::fs::PermissionsExt;
 use std::path::Path;
 
 use anyhow::Result;
+use crossterm::event::{Event, KeyCode, read};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 
 const BUILTINS: [&str; 5] = ["echo", "exit", "type", "cd", "pwd"];
 
@@ -21,11 +23,14 @@ fn push_completed(completed: &str, current_input: &mut String) {
 }
 
 pub fn autocomplete(current_input: &mut String) -> Result<()> {
+    let mut potential_matches: Vec<String> = vec![];
+
     // First check builtins
     for builtin in BUILTINS {
-        if builtin.starts_with(current_input.as_str()) {
-            push_completed(builtin, current_input);
-            return Ok(());
+        if builtin.starts_with(current_input.as_str())
+            && !potential_matches.contains(&builtin.to_string())
+        {
+            potential_matches.push(builtin.to_string());
         }
     }
 
@@ -37,22 +42,39 @@ pub fn autocomplete(current_input: &mut String) -> Result<()> {
         if dir_path.exists() {
             for entry in (read_dir(dir_path)?).flatten() {
                 let entry_file = entry.file_name();
-                let entry_str = entry_file
-                    .to_str()
-                    .expect("File name should have no trouble going to str");
+                let entry_str = entry_file.into_string().unwrap();
                 if entry_str.starts_with(current_input.as_str()) {
                     let permissions = entry.metadata()?.permissions();
                     let is_executable = permissions.mode() & 0o111 != 0;
-                    if is_executable {
-                        push_completed(entry_str, current_input);
-                        return Ok(());
+                    if is_executable && !potential_matches.contains(&entry_str) {
+                        potential_matches.push(entry_str);
                     }
                 }
             }
         }
     }
 
-    print!("\x07");
-    io::stdout().flush().expect("Could not flush bell");
+    if potential_matches.len() == 1 {
+        push_completed(potential_matches.first().unwrap(), current_input);
+    } else if potential_matches.len() > 1 {
+        print!("\x07");
+        io::stdout().flush().expect("Could not flush bell");
+        if let Ok(Event::Key(key_event)) = read()
+            && key_event.code == KeyCode::Tab
+        {
+            potential_matches.sort();
+            let potential_commands = potential_matches.join("  ");
+            disable_raw_mode()?;
+            println!();
+            println!("{potential_commands}");
+            print!("$ {current_input}");
+            io::stdout().flush().expect("Could not flush bell");
+            enable_raw_mode()?;
+        }
+    } else {
+        print!("\x07");
+        io::stdout().flush().expect("Could not flush bell");
+    }
+
     Ok(())
 }
